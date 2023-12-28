d4bd300a700943b971fefec5904b81bf3bc829cf
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2023-12-29 00:30:58 +0500

    Fix ini.js parsing

--- frontend/src/modules/scoleAPI/iniParsing/yearTimings.ts
+++ frontend/src/modules/scoleAPI/iniParsing/yearTimings.ts
@@ -30,37 +30,44 @@ export function parseYearTimings(script: string): YearTimings {
         firstQuarter: {
             name: STPERArray[0][1],
             start: parseDate(STPERArray[0][2]),
-            end: parseDate(STPERArray[0][3])
+            end: parseDate(STPERArray[0][3]),
+            originalDates: [STPERArray[0][2], STPERArray[0][3]]
         },
         secondQuarter: {
             name: STPERArray[1][1],
             start: parseDate(STPERArray[1][2]),
-            end: parseDate(STPERArray[1][3])
+            end: parseDate(STPERArray[1][3]),
+            originalDates: [STPERArray[1][2], STPERArray[1][3]]
         },
         firstSemester: {
             name: STPERArray[2][1],
             start: parseDate(STPERArray[2][2]),
-            end: parseDate(STPERArray[2][3])
+            end: parseDate(STPERArray[2][3]),
+        originalDates: [STPERArray[2][2], STPERArray[2][3]]
         },
         thirdQuarter: {
             name: STPERArray[3][1],
             start: parseDate(STPERArray[3][2]),
-            end: parseDate(STPERArray[3][3])
+            end: parseDate(STPERArray[3][3]),
+            originalDates: [STPERArray[3][2], STPERArray[3][3]]
         },
         fourthQuarter: {
             name: STPERArray[4][1],
             start: parseDate(STPERArray[4][2]),
-            end: parseDate(STPERArray[4][3])
+            end: parseDate(STPERArray[4][3]),
+            originalDates: [STPERArray[4][2], STPERArray[4][3]]
         },
         secondSemester: {
             name: STPERArray[5][1],
             start: parseDate(STPERArray[5][2]),
-            end: parseDate(STPERArray[5][3])
+            end: parseDate(STPERArray[5][3]),
+            originalDates: [STPERArray[5][2], STPERArray[5][3]]
         },
         year: {
             name: STPERArray[6][1],
             start: parseDate(STPERArray[6][2]),
-            end: parseDate(STPERArray[6][3])
+            end: parseDate(STPERArray[6][3]),
+            originalDates: [STPERArray[6][2], STPERArray[6][3]]
         }
     };
 }
